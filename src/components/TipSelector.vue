beef3b46944c7e9eb1e125b661d362f857a2dcd5
András Morauszki <morauszki.andras@gmail.com>
2025-03-22 15:30:24 +0100

    Text input for custom option

--- src/components/TipSelector.vue
+++ src/components/TipSelector.vue
@@ -1,15 +1,41 @@
 <script setup>
-const tipOptions = [5, 10, 15, 25, 50];
+import { ref } from "vue";
+const tipOptions = ["5", "10", "15", "25", "50"];
+const selectedOption = ref("5");
+const customTip = ref("Custom");
 </script>
 
 <template>
+  <p>Selected: {{ selectedOption }}</p>
   <fieldset>
     <template v-for="value in tipOptions" :key="value">
-      <input type="radio" name="tip" :value="value" :id="value" />
+      <input
+        type="radio"
+        name="tip"
+        :value="value"
+        :id="value"
+        v-model="selectedOption"
+      />
       <label :for="value">{{ value + "%" }}</label>
     </template>
-    <input type="radio" name="tip" :value="custom" id="custom" />
-    <label for="custom">Custom</label>
+    <input
+      type="radio"
+      name="tip"
+      value="custom"
+      id="custom"
+      v-model="selectedOption"
+    />
+    <label for="custom">
+      <input
+        type="text"
+        @click="
+          () => {
+            selectedOption = 'custom';
+          }
+        "
+        :value="customTip"
+      />
+    </label>
   </fieldset>
 </template>
 
