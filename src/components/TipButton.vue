234aa50fe7fcd4b5f1b6fce0c1fc3e43629aa1af
András Morauszki <morauszki.andras@gmail.com>
2025-08-05 15:58:32 +0200

    TipButton component

---
+++ src/components/TipButton.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { defineProps, defineEmits } from "vue";
+
+const { value, modelValue } = defineProps({
+  value: String,
+  modelValue: String,
+});
+
+const emit = defineEmits(["update:modelValue"]);
+
+function updateValue() {
+  emit("update:modelValue", value);
+}
+</script>
+
+<template>
+  <input
+    type="radio"
+    name="tip"
+    :id="value"
+    :value="value"
+    :checked="modelValue === value"
+    @change="updateValue"
+  />
+  <label :for="value">{{ value + "%" }}</label>
+</template>
--- src/components/TipSelector.vue
+++ src/components/TipSelector.vue
@@ -1,28 +1,28 @@
 <script setup>
 import { computed, ref, watch } from "vue";
 import { store } from "../store/store";
+import TipButton from "./TipButton.vue";
+
 const tipOptions = ["5", "10", "15", "25", "50"];
 const selectedOption = ref("");
 const customTip = ref("");
+
 const tipPcent = computed(() => {
   if (tipOptions.includes(selectedOption.value)) return selectedOption.value;
   return customTip.value;
 });
+
 watch(tipPcent, () => (store.tip = tipPcent));
 </script>
 
 <template>
   <fieldset>
-    <template v-for="value in tipOptions" :key="value">
-      <input
-        type="radio"
-        name="tip"
-        :value="value"
-        :id="value"
-        v-model="selectedOption"
-      />
-      <label :for="value">{{ value + "%" }}</label>
-    </template>
+    <TipButton
+      v-for="value in tipOptions"
+      :key="value"
+      :value="value"
+      v-model="selectedOption"
+    />
     <input
       type="text"
       @click="
